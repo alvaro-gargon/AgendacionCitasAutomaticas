7b1c7201ce11c21a4186129866425cab63a227f7
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2026-03-11 15:38:40 +0100

    add - changes

--- view/vFormulario.php
+++ view/vFormulario.php
@@ -1,7 +1,14 @@
 <form action="index.php?action=guardar" method="POST">
-    <input type="date" name="fecha" required>
-    <input type="time" name="hora" required>
-    <input type="text" name="asunto" required>
-    <textarea name="observaciones"></textarea>
-    <button type="submit">Guardar</button>
+    <p>
+        <input type="datetime-local" name="fechayhora" required>
+    </p>
+    <p>
+        <input type="text" name="asunto" required>
+    </p>
+    <p>
+        <textarea name="observaciones"></textarea>
+    </p>
+    <p>
+        <button type="submit">Guardar</button>
+    </p>
 </form>
