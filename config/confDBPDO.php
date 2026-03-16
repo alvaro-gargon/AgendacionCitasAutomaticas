53752360a8a901e0aebda08bfbb139a756433c28
alejandrohuerga <alejandrohuerga.dev@gmail.com>
2026-03-16 13:06:57 +0100

    ADD: Funcionalidad invitación Apple

--- config/confDBPDO.php
+++ config/confDBPDO.php
@@ -1,6 +1,6 @@
 <?php
     //archivo que sire para conectarse a la base de datos
-    define('DNS', 'mysql:host=localhost;dbname=agendacioncitasautomaticasdb');
+    define('DNS', 'mysql:host=localhost;dbname=agendacincitasautomaticasdb');
     define('USERNAME', 'root');
     define('PASSWORD', '');
 
